2ebaa1df16404d4091bfaedf822942e4f790c92c
Arnav Dalal <adalal542@berkeley.edu>
2025-05-21 20:08:39 +0000

    simple DLA added for cnn

--- train_ot_manifold_buffer_cnn.py
+++ train_ot_manifold_buffer_cnn.py
@@ -97,7 +97,7 @@ def get_tasks():
             tasks  = [DataLoader(PermutedCIFAR10(cifar_base,p), batch_size=BATCH,
                                 shuffle=True, drop_last=True) for p in perms]
         else:
-            temp = torch.randperm(10)
+            temp = torch.arange(10)
             perms  = [[temp[i], temp[i+1]] for i in range(0, len(temp)-1, 2)]
             tasks  = [DataLoader(SplitCIFAR10(cifar_base,p), batch_size=BATCH,
                                     shuffle=True, drop_last=True) for p in perms]
@@ -252,9 +252,111 @@ class OTHead(nn.Module):
         scr = z @ Pn.t() / TAU
         Q   = F.softmax(scr / TAU, dim=1)
         #ot  = -(Q * F.log_softmax(scr, dim=1)).sum(1).mean()
-        idx = Q.argmax(1)
-        cent = self.P[idx]
+        #idx = Q.argmax(1)
+        cent = Q @ self.P
         return cent
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Root(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1):
+        super(Root, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size,
+            stride=1, padding=(kernel_size - 1) // 2, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, xs):
+        x = torch.cat(xs, 1)
+        out = F.relu(self.bn(self.conv(x)))
+        return out
+
+
+class Tree(nn.Module):
+    def __init__(self, block, in_channels, out_channels, level=1, stride=1):
+        super(Tree, self).__init__()
+        self.root = Root(2*out_channels, out_channels)
+        if level == 1:
+            self.left_tree = block(in_channels, out_channels, stride=stride)
+            self.right_tree = block(out_channels, out_channels, stride=1)
+        else:
+            self.left_tree = Tree(block, in_channels,
+                                  out_channels, level=level-1, stride=stride)
+            self.right_tree = Tree(block, out_channels,
+                                   out_channels, level=level-1, stride=1)
+
+    def forward(self, x):
+        out1 = self.left_tree(x)
+        out2 = self.right_tree(out1)
+        out = self.root([out1, out2])
+        return out
+
+
+class SimpleDLA(nn.Module):
+    def __init__(self, block=BasicBlock, num_classes=10):
+        super(SimpleDLA, self).__init__()
+        self.base = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        )
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True)
+        )
+
+        self.layer3 = Tree(block,  32,  64, level=1, stride=1)
+        self.layer4 = Tree(block,  64, 128, level=2, stride=2)
+        self.layer5 = Tree(block, 128, 256, level=2, stride=2)
+        self.layer6 = Tree(block, 256, 512, level=1, stride=2)
+        #self.linear = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        out = self.base(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        #out = self.linear(out)
+        return out
 
 class Net(nn.Module):
     def __init__(self):
@@ -288,21 +390,29 @@ class Net(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
-        inp = 4096
+        # inp = 4096
+        # self.backbone = nn.Sequential(
+        #     nn.Dropout(0.1),
+        #     nn.Linear(inp, 512), nn.ReLU(),
+        #     nn.Linear(512, 256), nn.ReLU(),
+        #     nn.Dropout(0.1),
+        #     nn.Linear(256, D_CLS + D_TSK)
+        # )
         self.backbone = nn.Sequential(
-            nn.Dropout(0.1),
-            nn.Linear(inp, 512), nn.ReLU(),
             nn.Linear(512, 256), nn.ReLU(),
-            nn.Dropout(0.1),
             nn.Linear(256, D_CLS + D_TSK)
         )
+        self.input_blk = SimpleDLA()
         self.head = OTHead(D_TSK, K=15)
         self.fc   = nn.Linear(D_CLS + D_TSK, 10)
     def forward(self, x, replay_z=None):
-        z_imm = self.cnn(x)
-        z_flat = z_imm.view(z_imm.size(0), -1)
-        z = self.backbone(z_flat)
-
+        # z_imm = self.cnn(x)
+        # z_flat = z_imm.view(z_imm.size(0), -1)
+        # z = self.backbone(z_flat)
+        z_imm = self.input_blk(x)
+        #print(z_imm.shape)
+        z = self.backbone(z_imm)
+        #print(z.shape)
         zc = F.normalize(z[:, :D_CLS], p=2, dim=1)
         zt = F.normalize(z[:, D_CLS:], p=2, dim=1)
         cent = self.head(zt if replay_z is None else replay_z)
@@ -443,7 +553,13 @@ with torch.no_grad():
         idx = rng.choice(len(x_all), size=min(n_vis, len(x_all)), replace=False)
         x, y = x_all[idx].to(device), y_all[idx].to(device)
 
-        z = student.backbone(x)                     # [B, d_cls+d_tsk]
+        # z_imm = student.cnn(x)
+        # z_flat = z_imm.view(z_imm.size(0), -1)
+        # z = student.backbone(z_flat)
+        #z = student.backbone(x)                     # [B, d_cls+d_tsk]
+        z_imm = student.input_blk(x)
+        z = student.backbone(z_imm)
+
         Zc = F.normalize(z[:, :D_CLS], dim=1).cpu() # [B, d_cls]
         Zt = F.normalize(z[:, D_CLS:], dim=1).cpu() # [B, d_tsk]
 
